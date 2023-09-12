94d6f326c354d413b8fc8a3274009ec6eec5c316
Michael Kevin <michaelkmb@hotmail.com>
2023-09-12 15:21:57 -0300

    ~ optional dependency n exclusion;

---
+++ install_test/src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import one.digitalinnovation.component.Parser;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Parser parser = new Parser();
+        parser.message();
+    }
+}
